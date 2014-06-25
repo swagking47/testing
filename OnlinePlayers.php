f79785ef9fdfb23be6259f03e2e4e6adb11c05a6
PEMapModder <pemapmodder1970@gmail.com>
2014-06-25 16:04:54 +0800

    Update OnlinePlayers.php

--- OnlinePlayers.php
+++ OnlinePlayers.php
@@ -19,21 +19,22 @@ $this->api = $api;
 
 public function init(){
 $this->api->addHandler("tile.update",array($this,"eventHandler"));
-$this->api->schedule(1*1,array($this,"UpdateSign"),array(),true);
+$this->api->schedule(20,array($this,"UpdateSign"),array(),true); // once a second, or this will lag the server. TODO: move to andler of player.quit and player.spawn
 }
 public function eventHandler($data){
-            
+            $max = ServerAPI::request()->maxClients;
             if ($data->class === TILE_SIGN) {
                if ($data->data['Text1'] == "[OnlinePlayers]"){
            $players = $this->api->player->online();
-               $data->data["Text2"] = "[".count($players)."/100]";
+               $data->data["Text2"] = "[".count($players)."/$max]";
                              
              }
             }
 }
 public function UpdateSign(){
+            $max = ServerAPI::request()->maxClients;
             if(isset($this->sign)){
-               $data->data["Text2"] = "[".count($players)."/100]";         
+               $data->data["Text2"] = "[".count($players)."/$max]";         
 }
 }
 
